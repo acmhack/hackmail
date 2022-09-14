c67691c7b776395e911aa2de3997e970ffc1df46
Reid Haegele <rahdb4@umsystem.edu>
2022-09-14 15:53:52 -0500

    Form inputs

--- hackmail-app/src/App.js
+++ hackmail-app/src/App.js
@@ -6,17 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+            <label>
+                Company Email:
+                <input type="text" name="name" />
+            </label>
+            <label>
+                Company Name:
+                <input type="text" name="name" />
+            </label>
+            <label>
+                Body Text:
+                <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
